abf1a8259303bda26ff97ac7cd69961e3b53d716
frankkhiem <khiem.ngg@gmail.com>
2022-05-08 15:04:07 +0700

    test

--- src/components/friendsPage/SubSidebar.vue
+++ src/components/friendsPage/SubSidebar.vue
@@ -1,6 +1,21 @@
 <template>
 	<div>
-		123abc
+		<div class="section section--top">
+			<div class="section__title">
+				Danh mục
+			</div>
+			<div class="section__categories">
+				<div class="find-friends actived">
+					Tìm kiếm bạn bè
+				</div>
+				<div class="friends-request">
+					Lời mời kết bạn
+				</div>
+				<div class="friends-list">
+					Danh sách bạn bè
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -9,5 +24,28 @@
 </script>
 
 <style lang="scss" scoped>
-	
+.section {
+	padding: 1rem 0;
+
+	&__title {
+		margin-bottom: 1rem;
+		padding-left: 1rem;
+	}
+
+	&__categories {
+		> div {
+			padding: .6rem 1rem;
+			font-size: 18px;
+			cursor: pointer;
+
+			&:hover {
+				background-color: #091e4214;
+			}
+
+			&.actived {
+				background-color: #e4f0f6;
+			}
+		}
+	}
+}
 </style>
